d7232deb02c7790837c1748f5daff228b849b965
dzhuang <dzhuang.scut@gmail.com>
2020-06-20 23:41:59 +0800

    Allow push to branch repo.

--- run-build-and-tests.sh
+++ run-build-and-tests.sh
@@ -50,7 +50,14 @@ python manage.py collectstatic
 if [[ $DOCKER_USERNAME ]]; then
 echo "----Building docker image.----"
 cd "$CWD" || exit 1
-docker build --no-cache . -t $DOCKER_USERNAME/latex2image:${TRAVIS_COMMIT::8} || exit 1
+
+IMAGE=$DOCKER_USERNAME/latex2image
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  IMAGE=$DOCKER_USERNAME/latex2image-$TRAVIS_BRANCH
+fi
+
+docker build --no-cache . -t $IMAGE:${TRAVIS_COMMIT::8} || exit 1
 
 echo "----Docker images----"
 docker images
